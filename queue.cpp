4cdb6157a85b3f7f619bf1e4e44b4860de7face4
siizg <sa_surkova@mail.ru>
2023-03-09 22:08:09 +0300

    task3

--- queue.cpp
+++ queue.cpp
@@ -4,10 +4,19 @@
 #include<condition_variable>
 
 using namespace std;
+struct Node { 
+    int value; 
+    Node* next; 
+    
+    Node(int x) {
+        value = x;
+    }
+};
 
 class Queue {
     private:
-    queue <int> q;
+    Node* head;
+    Node* tail;
     int s;
     mutex mtx;
     condition_variable cond;
@@ -16,10 +25,25 @@ class Queue {
     Queue() {
         s = 0;
     }
+    
+    Queue(int x) {
+        head = new Node(x);
+        tail = head;
+        s = 1;
+    }
 
     void put(int x) {
         mtx.lock();
-        q.push(x);
+        Node* n = new Node(x);
+        
+        if(s == 0) {
+            head = n;
+            tail = n;
+        }
+        else {
+            tail->next = n; 
+            tail = n;
+        }
         s++;
         cond.notify_one();
         mtx.unlock();
@@ -27,15 +51,32 @@ class Queue {
 
     int pop() {
         mtx.lock();
-        int e = q.front();
-        q.pop();
+        if(s == 0) {
+            mtx.unlock();
+            return -1;
+        }
+        
+        if(s == 1) {
+            int value = head -> value;
+            s--;
+            delete head;
+            delete tail;
+            mtx.unlock();
+            return value;
+        }
+        
         s--;
+        Node* prev_head = head; 
+        head = head->next; 
+        int value = prev_head->value;
+        delete prev_head;
+        
         mtx.unlock();
-        return e;
+        return value;
     }
 
     size_t size() {
-        return (size_t)q.size();
+        return (size_t)s;
     }
     
     int wait_and_pop() {
